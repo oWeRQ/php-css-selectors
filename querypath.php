beab50f94da8b4be1e021521a34f459b4e1d29ca
Maxim Soldatov <ms@nowmedia.ru>
2016-09-23 16:15:32 +0300

    implement querypath parse

---
+++ querypath.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use QueryPath\CSS\Selector;
+use QueryPath\CSS\Parser;
+
+$handler = new Selector();
+$parser = new Parser('div.item > h4 > a', $handler);
+$parser->parse();
+
+var_dump($handler);
