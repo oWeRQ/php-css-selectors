084a75a851c533c2339dcd9e0820635ddabdebed
Maxim Soldatov <ms@nowmedia.ru>
2016-09-23 16:03:27 +0300

    init

---
+++ symfony.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Symfony\Component\CssSelector\CssSelectorConverter;
+
+$converter = new CssSelectorConverter();
+var_dump($converter->toXPath('div.item > h4 > a'));
